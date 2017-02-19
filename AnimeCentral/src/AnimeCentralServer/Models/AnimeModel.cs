2cdf35500a2c60286da9bfeb38fc4cb176e0d870
PhoenixItachi <juventus09deva@yahoo.com>
2017-02-19 17:53:41 +0200

    Changes to the server.

---
+++ AnimeCentral/src/AnimeCentralServer/Models/AnimeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeCentralServer.Models
+{
+    public class AnimeModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Range(1, 2000)]
+        public int NoEpisodes { get; set; }
+
+        [Required]
+        public string Synonyms { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Required]
+        public string Synopsis { get; set; }
+    }
+}
